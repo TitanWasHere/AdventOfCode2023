fbf1bb17a76db51a98e5c500a388d7c974d7de73
TitanWasHere <andrea.gravili8@gmail.com>
2023-12-10 15:08:41 +0100

    finished day 10/12/2023

--- 2_12_2023.py
+++ 2_12_2023.py
@@ -118,6 +118,11 @@ Game 99: 2 blue, 1 red, 9 green; 8 red, 1 blue, 1 green; 2 red, 7 green, 8 blue;
 Game 100: 3 blue, 6 red, 9 green; 4 red, 3 green; 4 green, 16 red, 1 blue; 14 blue, 1 green
 """.splitlines()
 
+input2 = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".splitlines()
 
 def part1(input):
     tot = 0
@@ -162,5 +167,34 @@ def part1(input):
 
     return tot
 
+def part2(input):
+    tot = 0
+    for line in input:
+        maxRed = 0
+        maxGreen = 0
+        maxBlue = 0
+        number = line.split(": ") # ["Game n", "xxx , xxx , xxx ; xxx , xxx"]
+        # Per avere il numero del game
+        #print(number[0][-1])
+
+        games = number[1].split(";")
+        print(games)
+
+        for game in games:
+            values = game.split(", ")
+
+            for val in values:
+                gem = val.split()
+                if gem[1] == "red":
+                    maxRed = max( int(gem[0]), maxRed)
+                elif gem[1] == "blue":
+                    maxBlue = max(int(gem[0]), maxBlue )
+                else:
+                    maxGreen = max(int(gem[0]), maxGreen)
+                
+        tot = tot + (maxRed * maxBlue * maxGreen)
+
+    return tot
+
 print(part1(input))
-#print(part2(input))
+print(part2(input))
