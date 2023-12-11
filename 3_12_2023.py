53527008edf6fbe7886cb0fa75676eb30b75252c
TitanWasHere <andrea.gravili8@gmail.com>
2023-12-11 12:26:28 +0100

    part 2 of third day completed

--- 3_12_2023.py
+++ 3_12_2023.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 
@@ -164,4 +165,39 @@ def part1(input):
     
     return part_numbers
 
+def part2(input):
+    parts_coordinates = {}
+    for x, line in enumerate(input_data):
+        for number in re.finditer(r'\d+', line):
+            left = number.start() - 1
+            right = number.end()
+            adjacent_characters = \
+                substring(x-1, left, right + 1) + \
+                substring(x, left, left + 1) + \
+                substring(x, right, right + 1) + \
+                substring(x+1, left, right + 1)
+            is_part_number = set(adjacent_characters) != {'.'}
+
+            if is_part_number:
+                part_number = int(number.group())
+                for y in range(number.start(), number.end()):
+                    parts_coordinates[(x, y)] = part_number
+
+    gear_ratios = []
+    for gear_x, line in enumerate(input_data):
+        for gear in re.finditer('\*', line):
+            gear_y = gear.start()
+            adjacent_coordinates = [(x, y) for x in range(gear_x-1, gear_x+2) for y in range(gear_y-1, gear_y+2)
+                                    if (x, y) != (gear_x, gear_y)
+                                    and x >= 0 and x < len(input_data)
+                                    and y >= 0 and y < len(line)]
+            adjacent_parts = set([parts_coordinates[x_y]
+                                for x_y in adjacent_coordinates if x_y in parts_coordinates])
+
+            if len(adjacent_parts) == 2:
+                gear_ratios.append(math.prod(adjacent_parts))
+
+    return gear_ratios
+
 print(sum(part1(input_data)))
+print(sum(part2(input_data)))
