c083a192c0bf17b8ef53cfd6662fb62171b039f4
TitanWasHere <andrea.gravili8@gmail.com>
2023-12-21 19:38:30 +0100

    finished day 8

--- 8_12_2023.py
+++ 8_12_2023.py
@@ -792,5 +792,25 @@ def part1(input):
 
     return steps
     
+def part2(input):
+    directions, connections = input.split('\n\n')
+    directions = [0 if d == 'L' else 1 for d in directions]
+    graph = {}
+    regex = r'(\w{3}) = \((\w{3}), (\w{3})\)'
+    for node, left, right in re.findall(regex, connections):
+        graph[node] = [left, right]
+
+    starting_nodes = [node for node in graph if node[2] == 'A']
+    cycles = []
+    for node in starting_nodes:
+        for steps, d in enumerate(cycle(directions), start=1):
+            node = graph[node][d]
+            if node[2] == 'Z':
+                cycles.append(steps)
+                break
+
+    return math.lcm(*cycles)
+
 
 print(part1(input))
+print(part2(input))
